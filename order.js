2c1a59cd0b3e1b9ac4d5c87f140eb462a76d89ae
Prerna Malik <prernamalik701@gmail.com>
2024-04-02 22:44:29 +0530

    first

---
+++ order.js
@@ -0,0 +1,49 @@
+var instagramIcon1 = document.getElementById('instagramIcon1');
+
+// Add a click event listener to the Facebook icon
+instagramIcon1.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.instagram.com';
+});
+var instagramIcon2 = document.getElementById('instagramIcon2');
+
+// Add a click event listener to the Facebook icon
+instagramIcon2.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.instagram.com';
+});
+var instagramIcon3 = document.getElementById('instagramIcon3');
+
+// Add a click event listener to the Facebook icon
+instagramIcon3.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.instagram.com';
+});
+var instagramIcon4 = document.getElementById('instagramIcon4');
+
+// Add a click event listener to the Facebook icon
+instagramIcon4.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.instagram.com';
+});
+var facebookIcon1 = document.getElementById('facebookIcon1');
+
+// Add a click event listener to the Facebook icon
+facebookIcon1.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.facebook.com';
+});
+var facebookIcon2 = document.getElementById('facebookIcon2');
+
+// Add a click event listener to the Facebook icon
+facebookIcon2.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.facebook.com';
+});
+var facebookIcon3 = document.getElementById('facebookIcon3');
+
+// Add a click event listener to the Facebook icon
+facebookIcon3.addEventListener('click', function() {
+    // Navigate to the Facebook page when the icon is clicked
+    window.location.href = 'https://www.facebook.com';
+});
---
+++ script.js
@@ -0,0 +1,152 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get references to the menu links
+    const homeLink = document.querySelector('a[href="#Home"]');
+    const aboutusLink = document.querySelector('a[href="#About"]');
+    const reviewLink = document.querySelector('a[href="#Review"]');
+    const menuLink = document.querySelector('a[href="#Menu"]');
+    const orderLink = document.querySelector('a[href="#Order"]');
+    const galaryLink = document.querySelector('a[href="#Gallary"]');
+
+    // Define a function to open a file in a new tab
+    function openFileInNewTab(file) {
+        window.open(file, '_blank');
+    }
+
+    // Add click event listeners to the menu links
+    homeLink.addEventListener('click', function() {
+        openFileInNewTab('home.html');
+    });
+
+    aboutusLink.addEventListener('click', function() {
+        openFileInNewTab('about-us.html');
+    });
+
+    reviewLink.addEventListener('click', function() {
+        openFileInNewTab('review.html');
+    });
+
+    menuLink.addEventListener('click', function() {
+        openFileInNewTab('menu.html');
+    });
+
+    orderLink.addEventListener('click', function() {
+        openFileInNewTab('order.html');
+    });
+
+    // Add click event listener for the Gallary link
+    galaryLink.addEventListener('click', function() {
+        openFileInNewTab('galary.html');
+    });
+
+    var facebookIcon = document.getElementById('facebookIcon');
+
+    // Add a click event listener to the Facebook icon
+    facebookIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.facebook.com';
+    });
+    var twitterIcon = document.getElementById('twitterIcon');
+
+    // Add a click event listener to the Facebook icon
+    twitterIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.twitter.com';
+    });
+    var instagramIcon = document.getElementById('instagramIcon');
+
+    // Add a click event listener to the Facebook icon
+    instagramIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.instagram.com';
+    });
+
+    var facebookIcon = document.getElementById('facebookIcon4');
+
+    // Add a click event listener to the Facebook icon
+    facebookIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.facebook.com';
+    });
+    var twitterIcon = document.getElementById('twitterIcon4');
+
+    // Add a click event listener to the Facebook icon
+    twitterIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.twitter.com';
+    });
+    var instagramIcon = document.getElementById('instagramIcon4');
+
+    // Add a click event listener to the Facebook icon
+    instagramIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.instagram.com';
+    });
+
+    ///////////////////
+    var facebookIcon = document.getElementById('facebookIcon3');
+
+    // Add a click event listener to the Facebook icon
+    facebookIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.facebook.com';
+    });
+    var twitterIcon = document.getElementById('twitterIcon3');
+
+    // Add a click event listener to the Facebook icon
+    twitterIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.twitter.com';
+    });
+    var instagramIcon = document.getElementById('instagramIcon3');
+
+    // Add a click event listener to the Facebook icon
+    instagramIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.instagram.com';
+    });
+    /////////////////////
+    var facebookIcon = document.getElementById('facebookIcon2');
+
+    // Add a click event listener to the Facebook icon
+    facebookIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.facebook.com';
+    });
+    var twitterIcon = document.getElementById('twitterIcon2');
+
+    // Add a click event listener to the Facebook icon
+    twitterIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.twitter.com';
+    });
+    var instagramIcon = document.getElementById('instagramIcon2');
+
+    // Add a click event listener to the Facebook icon
+    instagramIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.instagram.com';
+    });
+    ///////////
+    var facebookIcon = document.getElementById('facebookIcon1');
+
+    // Add a click event listener to the Facebook icon
+    facebookIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.facebook.com';
+    });
+    var twitterIcon = document.getElementById('twitterIcon1');
+
+    // Add a click event listener to the Facebook icon
+    twitterIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.twitter.com';
+    });
+    var instagramIcon = document.getElementById('instagramIcon1');
+
+    // Add a click event listener to the Facebook icon
+    instagramIcon.addEventListener('click', function() {
+        // Navigate to the Facebook page when the icon is clicked
+        window.location.href = 'https://www.instagram.com';
+    });
+
+});
